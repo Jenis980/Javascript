37ad815ba8af5c302697524ee2bc89a287e439f1
Jenis980 <110826725+Jenis980@users.noreply.github.com>
2023-11-26 21:45:35 +0545

    Update variables.js
    Use different keywords to declare constants and variables

---
+++ basic/variables.js
@@ -0,0 +1,20 @@
+const userId = 0001
+let userName = "James Gee"
+var userEmail = "jamesG@gmail.com"
+userAddress = "Toronto"
+
+// lets change value to investigate study
+// userId = 0002 // not possible 
+userName = "AmeetZk"
+userEmail = "whatever@gmail.com"
+userAddress = "New Delhi"
+// to peint on console
+console.log(userId)
+console.log(userName)
+console.log(userEmail)
+console.log(userAddress)
+
+/*
+also log can be shown in tabular form
+*/
+console.table([userId,userName,userEmail,userAddress])
