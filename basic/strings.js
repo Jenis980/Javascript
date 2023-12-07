422579ef7320ff79fa7ddd0f43969a34e3cb0c45
Jenis980 <110826725+Jenis980@users.noreply.github.com>
2023-12-07 15:27:54 +0000

    did some string functionusecase.

---
+++ basic/strings.js
@@ -0,0 +1,7 @@
+const nameOne = "Jk Rowl"
+const age = 80
+console.log(`My name is ${nameOne} and I’m ${age} years old `)
+const yelloVar = "         My name is Zyx. I am 55 years old.          "
+console.log(` length is:  ${yelloVar.length}`)
+console.log(yelloVar.toUpperCase())
+console.log(yelloVar.trim());
